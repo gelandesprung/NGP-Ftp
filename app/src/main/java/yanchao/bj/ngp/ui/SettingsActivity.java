da1e31bddab0805ac2b97cb5ff6ae6fc860f9e55
yanchao <gelandesprung@163.com>
2017-10-08 15:21:16 +0800

    创建设置界面

---
+++ app/src/main/java/yanchao/bj/ngp/ui/SettingsActivity.java
@@ -0,0 +1,19 @@
+package yanchao.bj.ngp.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import yanchao.bj.ngp.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
